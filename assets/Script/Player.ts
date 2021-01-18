a68163c0dd4dc465cbf3af66dc67f8327856e9cd
panpchen <panpchen@gmail.com>
2021-01-18 18:38:14 +0800

    player改动

--- assets/Script/Player.ts
+++ assets/Script/Player.ts
@@ -5,18 +5,21 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import { IPlayer } from "./GameData";
+import { IPlayer, PlayerData } from "./GameData";
 
 const { ccclass, property } = cc._decorator;
 @ccclass
 export default class Player extends cc.Component {
   @property(cc.Label)
   nameLabel: cc.Label = null;
+  @property(cc.Node)
+  selfArrow: cc.Node = null;
   private _data: IPlayer = null;
 
   init(data: IPlayer) {
     this._data = data;
     this.nameLabel.string = data.uname;
+    this.selfArrow.active = data.uname === PlayerData.uname;
     cc.log("玩家属性: ", this._data);
   }
 
